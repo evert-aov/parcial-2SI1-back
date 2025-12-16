a4ad3d204c0a5191f6879f7a399418a8bf90f4bf
evert-aov <evert.aov@gmail.com>
2025-12-16 01:11:53 -0400

    feat: add database seeding to startup script

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,8 @@
 echo "Running migrations..."
 php artisan migrate --force
 
+echo "Seeding database..."
+php artisan db:seed --force
+
 echo "Starting services..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
